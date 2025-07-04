44f1e5a63725fec1da5e24edb13afa45670d801c
nan <tsarsc@gmail.com>
2025-07-04 13:32:59 -0300

    :package: add o JavaFx no pom.xml

--- src/main/java/proj/provas/aplicacao/app/Main.java
+++ src/main/java/proj/provas/aplicacao/app/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Início do teste CRUD e Aplicação de Prova ===");
 
+
         // Serviços e Controllers
         TurmaServiceImpl turmaService = new TurmaServiceImpl();
         TurmaController turmaController = new TurmaController(turmaService);
