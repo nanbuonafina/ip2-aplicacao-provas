c6f7a635e8576442aa513da2d8f315f2b03bfae6
João <joao.victorferraz@ufrpe.br>
2025-07-06 16:33:54 -0300

    :wastebasket: removendo TelaInicial.fxml e TelaInicialController

--- src/main/java/proj/provas/aplicacao/app/Main.java
+++ src/main/java/proj/provas/aplicacao/app/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         // Carrega a tela inicial
-        Parent root = FXMLLoader.load(getClass().getResource("/TelaInicial.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/TelaDeLogon.fxml"));
 
         Scene scene = new Scene(root);
         stage.setTitle("Sistema de Provas");
