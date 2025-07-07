2b339f831751b799280ae4f52a86526c482f61b6
EvelinDionizio <evelin.dionizio@ufrpe.br>
2025-07-07 14:23:31 -0300

    🎨 style: Melhoria no design das telas

--- src/main/java/proj/provas/aplicacao/app/Main.java
+++ src/main/java/proj/provas/aplicacao/app/Main.java
@@ -6,14 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL; // Importe esta classe
+
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Carrega a tela inicial
-        Parent root = FXMLLoader.load(getClass().getResource("/TelaDeLogon.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/TelaDeLogon.fxml"));
+        Parent root = loader.load();
 
         Scene scene = new Scene(root);
+
+        URL cssResource = getClass().getResource("/css/style.css");
+        if (cssResource != null) {
+            scene.getStylesheets().add(cssResource.toExternalForm());
+            System.out.println("CSS carregado com sucesso: " + cssResource.toExternalForm()); // Opcional: para debug
+        } else {
+            System.err.println("Erro: Arquivo CSS não encontrado em /css/style.css");
+        }
+
         stage.setTitle("Sistema de Provas");
         stage.setScene(scene);
         stage.show();
