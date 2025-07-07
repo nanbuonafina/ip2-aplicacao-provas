38056199c1f81d34d853d7042c963e2ffafe6de9
João <joao.victorferraz@ufrpe.br>
2025-07-06 22:48:55 -0300

    Controller tela de logon

--- src/main/java/proj/provas/aplicacao/controller/TelaDeLogonController.java
+++ src/main/java/proj/provas/aplicacao/controller/TelaDeLogonController.java
@@ -1,4 +1,35 @@
 package proj.provas.aplicacao.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TelaDeLogonController {
+
+
+    @FXML
+    void abrirTelaDeCadastro(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("TelaDeAutoCadastro.fxml"));
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.setTitle("Cadastro de Usuário"); // Opcional: mudar o título da janela
+
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Erro ao carregar a tela de cadastro: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
